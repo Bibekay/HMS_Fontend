fb019f9e734d6b53862313e75ad5889a6b575bd9
bibekay <bibuayadi@gmail.com>
2020-07-04 13:57:38 +0545

    design changes in user info activity

--- app/src/main/java/com/example/hms/activities/admin/UserInfoActivity.java
+++ app/src/main/java/com/example/hms/activities/admin/UserInfoActivity.java
@@ -1,20 +1,27 @@
 package com.example.hms.activities.admin;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hms.R;
+import com.example.hms.activities.users.LoginActivity;
 import com.example.hms.adapter.UserInfoAdapter;
 import com.example.hms.api.Admin_api;
 import com.example.hms.models.Users;
@@ -28,7 +35,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class UserInfoActivity extends AppCompatActivity {
+public class UserInfoActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     UserInfoActivity activity;
     private RecyclerView recyclerView;
@@ -38,9 +45,8 @@ public class UserInfoActivity extends AppCompatActivity {
 
 
     static final float END_SCALE = 0.7f;
-    ImageView menuIcon;
     LinearLayout contentView;
-
+    ImageView menuIcon;
     //Drawer Menu
     DrawerLayout drawerLayout;
     NavigationView navigationView;
@@ -60,6 +66,15 @@ public class UserInfoActivity extends AppCompatActivity {
         back = findViewById(R.id.imgAdminBack);
         recyclerView = findViewById(R.id.users_list);
         searchusername = findViewById(R.id.admin_user_search_view);
+
+        //Menu Hooks
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        contentView = findViewById(R.id.content);
+        menuIcon = findViewById(R.id.menu_icon);
+        navigationDrawer();
+
 
 
 
@@ -88,6 +103,63 @@ public class UserInfoActivity extends AppCompatActivity {
         });
 
     }
+
+
+
+    private void navigationDrawer() {
+
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(activity);
+        navigationView.setCheckedItem(R.id.home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+
+                else drawerLayout.openDrawer(GravityCompat.START);
+
+            }
+        });
+
+        animateNavigationDrawer();
+    }
+
+    private void animateNavigationDrawer() {
+
+        //Add any color or remove it to use the default one!
+        //To make it transparent use Color.Transparent in side setScrimColor();
+        drawerLayout.setScrimColor(getResources().getColor(R.color.colorPrimary));
+
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else
+            super.onBackPressed();
+    }
+
     private void loaduserinfo() {
 
 
@@ -121,4 +193,50 @@ public class UserInfoActivity extends AppCompatActivity {
         startActivity(openadmindashboard);
     }
 
+
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                AlertDialog.Builder builder = new AlertDialog.Builder(UserInfoActivity.this);
+                builder.setCancelable(false);
+                builder.setMessage("Do you want to Logout?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //if user pressed "yes", then he is allowed to exit from application
+                        SharedPreferences sharedPreferences = UserInfoActivity.this.getSharedPreferences("IMS", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.remove("token");
+                        editor.remove("isadmin");
+                        editor.remove("status");
+                        editor.remove("username");
+                        editor.remove("password");
+                        editor.commit();
+                        url.token = "Bearer ";
+                        url.status = "Status";
+                        Intent i = new Intent(UserInfoActivity.this, LoginActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(i);
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+
+
+                break;
+
+
+        }
+        return true;
+    }
 }
