0696366cabd81171a03bf02b4c11ef4e0dd7c05e
bibekay <bibuayadi@gmail.com>
2020-07-28 00:06:27 +0545

    hotel recycler view design added

---
+++ app/src/main/java/com/example/hms/activities/users/UserviewhotelsActivity.java
@@ -0,0 +1,16 @@
+package com.example.hms.activities.users;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hms.R;
+
+public class UserviewhotelsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userviewhotels);
+    }
+}
